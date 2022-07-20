707be049758b27231435c5bae4d2b7f39164aee7
Jhalak Gope <jhalak.eco@gmail.com>
2022-07-20 04:10:03 +0200

    - web scraping performd in a basic foramt

--- web_scrapping_1.R
+++ web_scrapping_1.R
@@ -1,7 +1,21 @@
 library(rvest)
 library(dplyr)
 
-link = "https://twitter.com/jhalakgope"
-page = read_html(link)
+link <- "https://en.wikipedia.org/wiki/List_of_Nobel_laureates_in_Physics"
+page <- read_html(link)
 
-name = page %>% html_nodes()
+name <- page %>% 
+  html_nodes("tbody th a") %>%
+  html_text()
+country <- page %>% 
+  html_nodes("tr~ tr+ tr .flagicon+ a , tbody tr:nth-child(1) th a") %>%
+  html_text()
+
+
+de_country <- (country %>% 
+                 recode("West Germany" = "Germany"))
+de_country <- (country %>% 
+                 recode("Weimar Republic" = "Germany",
+                        "West Germany" = "Germany"))
+de <- de_country %>% 
+  filter(de_country,"Germany")
