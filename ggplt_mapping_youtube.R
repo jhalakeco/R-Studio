d96254fb177a37c4ab92c8eb3de63d7c02159b03
Jhalak Gope <jhalak.eco@gmail.com>
2022-07-30 23:59:11 +0200

    - new graps added

--- ggplt_mapping_youtube.R
+++ ggplt_mapping_youtube.R
@@ -1,6 +1,7 @@
 # Loading necessary libraries
 library(ggplot2)
 library(tidyverse)
+library(dplyr)
 
 # Reading rawdata file
 EUvax <- read.csv("C:/Users/jhala/OneDrive/Documents/My_projects/EUvaccine.csv")
@@ -10,5 +11,23 @@ View(EUvax)
 mapdata <- map_data("world")
 View(mapdata)
 
+
+
 # Combining mapdata and EUvax
 
+mapdata <- left_join(mapdata, EUvax, by = "region")
+View(mapdata)
+
+
+# filtering EU countries
+
+mapdata_eu <- (mapdata %>%
+                 filter(!is.na(mapdata$Perc_vaccinated)))
+View(mapdata_eu)
+
+
+# making a map
+
+map1 <- ggplot(mapdata_eu, aes(x=long, y=lat, group=group)) +
+  geom_polygon(aes(fill=Perc_vaccinated), color = "black")
+map1
