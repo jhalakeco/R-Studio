99ba1631c0d22e39a6146f3e3bc55d7498e0cfdc
Jhalak Gope <jhalak.eco@gmail.com>
2022-08-08 06:20:05 +0200

    - mapping done with several editing in the axes and theme

--- ggplt_mapping_youtube.R
+++ ggplt_mapping_youtube.R
@@ -1,33 +1,51 @@
 # Loading necessary libraries
-library(ggplot2)
 library(tidyverse)
-library(dplyr)
 
-# Reading rawdata file
+# Reading rawdata file ----
 EUvax <- read.csv("C:/Users/jhala/OneDrive/Documents/My_projects/EUvaccine.csv")
 View(EUvax)
 
-# Preparing coordinates
+# Preparing coordinates ----
 mapdata <- map_data("world")
 View(mapdata)
 
 
 
-# Combining mapdata and EUvax
-
+# Combining mapdata and EUvax ----
 mapdata <- left_join(mapdata, EUvax, by = "region")
 View(mapdata)
 
 
-# filtering EU countries
-
+# filtering EU countries ----
 mapdata_eu <- (mapdata %>%
                  filter(!is.na(mapdata$Perc_vaccinated)))
 View(mapdata_eu)
+# making a map ----
+map1 <- ggplot(mapdata_eu, aes(x=long, 
+                               y=lat, 
+                               group=group)) +
+  geom_polygon(aes(fill=Perc_vaccinated), color = "black") # ploygon for mapping, color is the border color between the countries
 
-
-# making a map
-
-map1 <- ggplot(mapdata_eu, aes(x=long, y=lat, group=group)) +
-  geom_polygon(aes(fill=Perc_vaccinated), color = "black")
 map1
+
+
+# making a new map to get rid of the axes and making it more coherent ----
+map2 <- map1 +
+  scale_fill_gradient(name= "% vaccinated",
+                      low = "#b5bde3",
+                      high = "#0b2183",
+                      na.value = "gray50") +
+  theme(axis.text.x = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        rect = element_blank())+
+  labs(title = "Percentage of vaccination in EU countries")
+
+map2
+
+
+
+
+
